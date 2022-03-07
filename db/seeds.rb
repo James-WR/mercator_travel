48e398d2c129d3105a8ec454050e7a48872e3a1d
James Robbs <jamesrobbs01@gmail.com>
2022-03-07 00:50:24 +0000

    seeded test trip

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create!(email: "jr@jr.com", password: "123456", username: "James123")
+puts "Created a user"
+Trip.create!(name: "my holiday", user_id: user.id)
+puts "Created a trip for this user"
