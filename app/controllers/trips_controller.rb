df7cab60f6d704017b256a7c4ae330822eb37d3e
James Robbs <jamesrobbs01@gmail.com>
2022-03-14 21:50:29 +0000

    new and create basic implementation

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -14,4 +14,24 @@ class TripsController < ApplicationController
   def show
     @trip = Trip.find(params[:id])
   end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.user = current_user
+    if @order.save!
+      redirect_to trip_path(@trip)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name, :destination_exact)
+  end
 end
