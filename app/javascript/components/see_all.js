45deeb87d9d421bf4c2cba082786072bcc73aa9e
James Robbs <jamesrobbs01@gmail.com>
2022-04-03 01:47:14 +0100

    columns in 'see all' box, style changes and sticky buttons

--- app/javascript/components/see_all.js
+++ app/javascript/components/see_all.js
@@ -2,13 +2,18 @@ const seeAllButton = document.querySelector("#see-all-button");
 const map = document.getElementById("map");
 const buttons = document.getElementById("buttons");
 const quotes = document.getElementById("quotes");
+const tripsList = document.getElementById("trips-list");
 
 seeAllButton.addEventListener("click", makeBoxBig)
 
 function makeBoxBig(e) {
   e.preventDefault();
+  this.classList.toggle("see-all-enlarged")
   const seeAllBox = this.parentElement
   seeAllBox.classList.toggle("make-box-big");
+  seeAllBox.style.overflow = "scroll";
+  tripsList.classList.toggle("two-column-trips");
+  seeAllBox.style.transition = "all 0.3s";
   map.style.zIndex = "0";
   buttons.classList.toggle("d-none");
   quotes.classList.toggle("d-none");
