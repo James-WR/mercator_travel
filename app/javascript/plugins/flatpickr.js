13199b63234cac1b9e28d34dbbc5ec9526bf6026
James Robbs <jamesrobbs01@gmail.com>
2022-04-17 16:27:16 +0100

    flatpickr.js tweak

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,7 @@
 // flatpickr.js provides a nicer UX for picking dates on a form.
 
 import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css"
+require("flatpickr/dist/flatpickr.min.css")
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 flatpickr("#start_date", {
